3114c41f3a0f9611a57fc1db325586a2a6dab79a
BugiGame-BGT <bugi.services@gmail.com>
2023-02-21 10:33:59 +0100

    finish lotery

--- kbrp-lottery/sh_language_fr.lua
+++ kbrp-lottery/sh_language_fr.lua
@@ -5,12 +5,12 @@ DarkRP.RegisterSentences("kbrp_lottery", "fr", {
 	["refuseButton"] = "Refuser",
 	["winTextTitle"] = "VOUS AVEZ REMPORTE",
 	["winTextDescription"] = "LA LOTERIE DE %s",
-	["playerWinChatText"] = "[KBLoterie] ",
-	["refundNotification"] = "Vous avez été remboursé de %s car il n'y avais pas assez de participants !",
+	["playerWinChatText"] = "[KBLoterie] %s",
+	["refundNotification"] = "Vous avez été remboursé de %s car il n'y avait pas assez de participants !",
 	["noLottery"] = "Il n'y a pas de loterie en cours !",
-	["cantParticipateTwice"] = "Vous ne pouvez participer qu'une seul fois à la loterie",
+	["cantParticipateTwice"] = "Vous ne pouvez participer qu'une seule fois à la loterie",
 	["walletPaidLottery"] = "Vous avez payé %s pour participer à la loterie !",
 	["walletAndBankPaidLottery"] = "Vous avez payé %s de votre poche et vous avez été débité de %s dans votre bancaire pour participer à la loterie",
-	["lotteryBankWithdrawLog"] = "Participation a la lotterie",
+	["lotteryBankWithdrawLog"] = "Participation a la loterie",
 	["cantAffordLottery"] = "Vous n'avez pas assez d'argent pour participer à la loterie !"
 })
