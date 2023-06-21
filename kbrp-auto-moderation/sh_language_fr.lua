18690352246cac527ac219f00b604ba1e9dd9f72
Marius Werther <122283253+Marius010506@users.noreply.github.com>
2023-06-21 18:41:06 +0200

    Update sh_language_fr.lua

--- kbrp-auto-moderation/sh_language_fr.lua
+++ kbrp-auto-moderation/sh_language_fr.lua
@@ -7,4 +7,5 @@ DarkRP.RegisterSentences("kbrp_auto_moderation", "fr", {
 	["tazzTooMuch"] = "Freetazz plus de %s fois des personnes au hasard",
 	["cantSerflexPlayerWithActiveWeapon"] = "La personne est armée vous ne pouvez pas lui mettre des colliers de sérrages",
 	["cantCuffPlayerWithActiveWeapon"] = "La personne est armée vous ne pouvez pas la menotter",
+	["cantHaveThisName"] = "Vous ne pouvez pas avoir ce nom RolePlay car il contient un mot interdit"
 })
