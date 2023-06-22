1e500922d6295c823db47ecd206cb79845e78e07
Marius Werther <122283253+Marius010506@users.noreply.github.com>
2023-06-22 13:10:28 +0200

    Update sh_language_de.lua

--- kbrp-car-dealer/sh_language_de.lua
+++ kbrp-car-dealer/sh_language_de.lua
@@ -73,6 +73,6 @@ DarkRP.RegisterSentences("kbrp_car_dealer", "de", {
 ["noEnoughtMoneyToRefuelVehicle"] = "Du hast nicht genug Geld um das Fahrzeug zu tanken!",
 ["vehicleRefuelled"] = "Du hast dein Fahrzeug betankt!",
 ["noEnoughtMoneyToRepairVehicle"] = "Du hast nicht genug Geld um das Fahrzeug zu reparieren!",
-["vehicleRepaired"] = "Du hast dein Fahrzeug repariert!"
+["vehicleRepaired"] = "Du hast dein Fahrzeug repariert!",
 ["payForCustomExitPrice"] = "Du hast gerade %s für %s bezahlt!"	
 })
