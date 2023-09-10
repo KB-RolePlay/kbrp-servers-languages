a4262392b8d8d5ea3d249ce78a1cb67cca032842
BarilTomas <115024933+BarilTomas@users.noreply.github.com>
2023-09-10 18:17:25 +0200

    Create sh_language_pt.lua

---
+++ kbrp-wallet/sh_language_pt.lua
@@ -0,0 +1,5 @@
+DarkRP.RegisterSentences("kbrp_wallet", "pt", {
+    ["frameTitle"] = "Porta-Moedas",
+    ["dropMoneyButton"] = "DEITAR DINHEIRO FORA",
+    ["textEntryPlaceholder"] = "Deitar dinheiro fora (voce tem %s)"
+})
